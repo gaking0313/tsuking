652f7a7cc827a773bc987bcf0a8b1c5801862787
kohei_ot <kohei_ot@192.168.11.4>
2017-08-03 01:09:51 +0900

    サービスクラスのログ出力追加(実際はAOPが差し込まれていない。。。)

--- src/main/java/common/aop/LoggingAop.java
+++ src/main/java/common/aop/LoggingAop.java
@@ -10,14 +10,8 @@ import org.springframework.stereotype.Component;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * AOPクラス.
- * <pre>
- * [概要]
  * 共通ロギング処理クラス。
- * [備考]
- * なし。
- * </pre>
- * 
+ *
  */
 @Slf4j
 @Aspect
@@ -25,53 +19,102 @@ import lombok.extern.slf4j.Slf4j;
 @EnableAspectJAutoProxy
 public class LoggingAop {
 
-	/**
-	 * Log Aspect.
-	 * <pre>
-	 * [概要]
-	 * ログ出力共通処理。
-	 * ポイントカットされた時に、開始と終了のログを出力する。
-	 * [備考]
-	 * ポイントカット = "com.example.demo.controller.*.*(..))"の部分。
-	 * 指定されている内容にマッチした場合、処理を行う。
-	 * コントローラークラスのログを出力する。
-	 * </pre>
-	 * @param joinPoint JoinPoint:ポイントカットされた処理。マッチした処理を行う等に使用できる
-	 * @return result:行った処理の結果オブジェクト
-	 * @throws Throwable Exception
-	 */
-	@Around("execution(* com.example.demo.controller.*.*(..))")
-	public Object logController(ProceedingJoinPoint joinPoint) throws Throwable {
+    /**
+     * ログ出力共通処理。
+     * コントローラークラスのログを出力する。
+     *
+     * @param joinPoint JoinPoint:ポイントカットされた処理。マッチした処理を行う等に使用できる
+     * @return result:行った処理の結果オブジェクト
+     * @throws Throwable Exception
+     */
+    @Around("execution(* com.example.demo.controller.*.*(..))")
+    public Object logController(ProceedingJoinPoint joinPoint)
+            throws Throwable {
 
-		// クラス名#メソッド名
-		String target = joinPoint.getTarget().getClass().toString() + "#" + joinPoint.getSignature().getName();
+        // クラス名#メソッド名
+        String target =
+            joinPoint.getTarget().getClass().toString()
+                + "#"
+                + joinPoint.getSignature().getName();
 
-		// Log
-		log.info(target + " ***** BEGIN *****");
-		log.info(target + " ***** INPUT PARAMETERS ***** params=" + ArrayUtils.toString(joinPoint.getArgs()));
+        // Log
+        log.info(target + " ***** BEGIN *****");
+        log.info(
+            target
+                + " ***** INPUT PARAMETERS ***** params="
+                + ArrayUtils.toString(joinPoint.getArgs()));
 
-		Object result = null;
-		try {
-			result = joinPoint.proceed(joinPoint.getArgs());
-			// resultがnullでない場合は内容を出力し、nullの場合はnullと出力する
-			if (result != null) {
-				// resultがリダイレクトURL（String型）の場合はそのまま出力
-				if (result instanceof String) {
-					log.info(target + " ***** OUTPUT ***** " + result);
-				} else {
-					log.info(target + " ***** OUTPUT ***** " + result.toString());
-				}
-			} else {
-				log.info(target + " ***** OUTPUT ***** " + null);
-			}
-		} catch (Throwable e) {
-			// サービスからのException
-			log.error(target + " ***** SYSTEM ***** ", e);
-			throw e;
-		} finally {
-			log.info(target + " ***** END *****");
-		}
-		return result;
-	}
+        Object result = null;
+        try {
+            result = joinPoint.proceed(joinPoint.getArgs());
+            // resultがnullでない場合は内容を出力し、nullの場合はnullと出力する
+            if (result != null) {
+                // resultがリダイレクトURL（String型）の場合はそのまま出力
+                if (result instanceof String) {
+                    log.info(target + " ***** OUTPUT ***** " + result);
+                } else {
+                    log.info(
+                        target + " ***** OUTPUT ***** " + result.toString());
+                }
+            } else {
+                log.info(target + " ***** OUTPUT ***** " + null);
+            }
+        } catch (Throwable e) {
+            // サービスからのException
+            log.error(target + " ***** SYSTEM ***** ", e);
+            throw e;
+        } finally {
+            log.info(target + " ***** END *****");
+        }
+        return result;
+    }
+
+    /**
+     * ログ出力共通処理。
+     * コントローラークラスのログを出力する。
+     *
+     * @param joinPoint JoinPoint:ポイントカットされた処理。マッチした処理を行う等に使用できる
+     * @return result:行った処理の結果オブジェクト
+     * @throws Throwable Exception
+     */
+    @Around("execution(* com.example.demo.service.*.*.*(..))")
+    public Object logService(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        // クラス名#メソッド名
+        String target =
+            joinPoint.getTarget().getClass().toString()
+                + "#"
+                + joinPoint.getSignature().getName();
+
+        // Log
+        log.info(target + " ***** BEGIN *****");
+        log.info(
+            target
+                + " ***** INPUT PARAMETERS ***** params="
+                + ArrayUtils.toString(joinPoint.getArgs()));
+
+        Object result = null;
+        try {
+            result = joinPoint.proceed(joinPoint.getArgs());
+            // resultがnullでない場合は内容を出力し、nullの場合はnullと出力する
+            if (result != null) {
+                // resultがリダイレクトURL（String型）の場合はそのまま出力
+                if (result instanceof String) {
+                    log.info(target + " ***** OUTPUT ***** " + result);
+                } else {
+                    log.info(
+                        target + " ***** OUTPUT ***** " + result.toString());
+                }
+            } else {
+                log.info(target + " ***** OUTPUT ***** " + null);
+            }
+        } catch (Throwable e) {
+            log.error(target + " ***** SYSTEM ***** ", e);
+            throw e;
+        } finally {
+            log.info(target + " ***** END *****");
+        }
+        return result;
+    }
 
 }
