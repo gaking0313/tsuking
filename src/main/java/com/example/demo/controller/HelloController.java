e7d56313bba1a21d7e3c5284e29102c8c8fffb7f
k_otsuki <otsuki.kohei@tis.co.jp>
2017-08-02 19:14:06 +0900

    不要なインポートを削除

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -11,8 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import ch.qos.logback.core.net.SyslogOutputStream;
-
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
